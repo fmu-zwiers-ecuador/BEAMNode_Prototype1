b6177370572ce8a6efbcecc389a5f03424e0e8c6
Pazmaz <162527327+Pazmazz@users.noreply.github.com>
2025-10-02 13:59:15 -0400

    Updated to fix file directory bug

--- scripts/tsl2591/log_lux_data.py
+++ scripts/tsl2591/log_lux_data.py
@@ -2,6 +2,7 @@ import board
 import adafruit_tsl2591
 import json
 from datetime import datetime
+import os
 
 # Create sensor object, communicating over the board's default I2C bus
 i2c = board.I2C()
@@ -11,17 +12,23 @@ sensor = adafruit_tsl2591.TSL2591(i2c)
 
 # Read and calculate the light level in lux.
 lux = sensor.lux
+
+directory = "/data/tsl2591/"
+
+# Create directory if it does not exist
+os.makedirs(directory, exist_ok=True)
     
 lux_json_data = {
-    "date": datetime.now().isoformat,
+    "date": datetime.now().isoformat(),
     "lux": lux
 }
 
 file_name = f"hourly_data_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
+file_path = os.path.join(directory, file_name)
 
 try:
-    with open(f"data/tsl2591/{file_name}", 'w') as f:
-        json.dump(lux_json_data, f, indent=4)
+    with open(file_path, 'w') as json_file:
+        json.dump(lux_json_data, json_file, indent=4)
     print(f"Lux data saved to {file_name} at {datetime.now()}")
 except Exception as e:
      print(f"Error saving lux data: {e}")
