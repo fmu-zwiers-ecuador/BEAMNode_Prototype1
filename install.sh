98bb03caf138968bcefb5f79ae9e8c0921eccb1e
ElAmoen3D <68702215+ElAmoen3D@users.noreply.github.com>
2025-10-13 12:16:27 -0400

    Updated install.sh, added camera module installation

--- install.sh
+++ install.sh
@@ -5,5 +5,12 @@ sudo pip3 install adafruit-circuitpython-bme280 --break-system-packages
 
 # install camera module
 
-sudo apt install picamzero --break-system-packages
+sudo apt update
+sudo apt install build-essential meson ninja-build pkg-config libyaml-dev python3-yaml python3-ply python3-jinja2 libssl-dev openssl git
 
+git clone https://git.libcamera.org/libcamera/libcamera.git
+cd libcamera
+
+meson setup build
+ninja -C build
+sudo ninja -C build install
