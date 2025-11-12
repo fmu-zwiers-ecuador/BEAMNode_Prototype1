057b06ea1c0571e1f86a1aad7e03423a63967d1b
ElAmoen3D <68702215+ElAmoen3D@users.noreply.github.com>
2025-11-12 11:36:49 -0500

    deleted camera stuff from install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,6 @@ sudo pip3 install adafruit-blinka --break-system-packages
 # install bme280 library
 sudo pip3 install adafruit-circuitpython-bme280 --break-system-packages
 
-# install camera module
-sudo apt update
-sudo apt install build-essential meson ninja-build pkg-config libyaml-dev python3-yaml python3-ply python3-jinja2 libssl-dev openssl git
-git clone https://git.libcamera.org/libcamera/libcamera.git
-cd libcamera
-meson setup build
-ninja -C build
-sudo ninja -C build install
-
 # install PyAudio library
 sudo apt install python3-pyaudio portaudio-dev -y
 sudo pip3 install pyaudio --break-system-packages
