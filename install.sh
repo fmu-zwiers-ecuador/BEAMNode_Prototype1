7081a3eac118be8d0929653f3df5a93e73f798a2
ElAmoen3D <68702215+ElAmoen3D@users.noreply.github.com>
2025-11-12 12:05:35 -0500

    added break system packages to prevent bugs, added update and upgrade

--- install.sh
+++ install.sh
@@ -12,15 +12,18 @@ sudo pip3 install pyaudio --break-system-packages
 
 # install TSL2591 library
 pip3 install adafruit-circuitpython-tsl2591 --break-system-packages
-sudo pip3 install adafruit-circuitpython-tsl2591
+sudo pip3 install adafruit-circuitpython-tsl2591 --break-system-packages
 
 # Installation for the original Python TSL2591 library
 pip install python-tsl2591
 
 # install AHXT0 library
 pip3 install adafruit-circuitpython-ahtx0 --break-system-packages
-sudo pip3 install adafruit-circuitpython-ahtx0
+sudo pip3 install adafruit-circuitpython-ahtx0 --break-system-packages
 
 # installing Batctl packages
 sudo apt install batctl
+
+#update and upgrade system
+sudo apt update && sudo apt upgrade 
 
